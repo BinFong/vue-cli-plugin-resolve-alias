023caba3eff7ab605df2b09e58d4bca2f80985aa
BinFong <bin.neverbug@gmail.com>
2018-11-27 21:32:38 +0800

    update index

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ module.exports = (api, projectOptions) => {
 	const options = projectOptions.pluginOptions['resolve-alias'];
 
 	api.chainWebpack(webpackConfig => {
-		const alias = options.alias;
-		// console.log('调试信息：', JSON.stringify(options));
-		console.log('调试信息：', Object.keys(alias));
-
+    const alias = options.alias;
+    
 		Object.keys(alias).forEach(key => {
 			if (alias[key]) {
 				webpackConfig.resolve.alias.set(key, alias[key]);
 			}
-		});
+    });
+    
 	});
 };
