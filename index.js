5ebe02aaab1406e70a8e701a6b2da260ed8b6481
Bin Fong <Bin.NeverBug@gmail.com>
2019-01-17 14:46:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 module.exports = (api, projectOptions) => {
 
-	const options = projectOptions.pluginOptions['resolve-alias'];
+  const options = projectOptions.pluginOptions['resolve-alias'];
 
-	api.chainWebpack(webpackConfig => {
+  api.chainWebpack(webpackConfig => {
 
     const alias = options.alias;
     
     Object.keys(alias).forEach(key => {
-			if (alias[key]) {
-				webpackConfig.resolve.alias.set(key, alias[key]);
-			}
+      if (alias[key]) {
+        webpackConfig.resolve.alias.set(key, alias[key]);
+      }
     });
-    
-	});
+
+  });
 };
